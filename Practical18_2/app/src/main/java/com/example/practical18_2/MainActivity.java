04aa09f1bbc5fb9f48d6523c59b7613acfdc4c64
VimalChaudhary07 <139368631+VimalChaudhary07@users.noreply.github.com>
2024-02-19 12:43:16 +0530

    Add files via upload

---
+++ Practical18_2/app/src/main/java/com/example/practical18_2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.practical18_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.bt1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9870032835"));
+                startActivity(intent);
+            }
+        });
+    }
+}
